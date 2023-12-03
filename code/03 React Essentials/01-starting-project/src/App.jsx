cd74e1bdd304698b64102744913d5c6af0800d19
n-brewster <nbrewster@netflix.com>
2023-12-02 17:48:14 -0800

    update

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 
 import { CORE_CONCEPTS } from "./data.js";
-import Header from './components/Header/Header.jsx'
+import { EXAMPLES } from "./data.js";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
-
-
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -13,13 +20,32 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section />
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic]}</h3>
+            <p>{EXAMPLES[selectedTopic]}</p>
+            <pre>
+              <code></code>
+            </pre>
+            
+          </div>
+        </section>
       </main>
     </div>
   );
--- code/03 React Essentials/01-starting-project/src/components/CoreConcept.jsx
+++ code/03 React Essentials/01-starting-project/src/components/CoreConcept.jsx
@@ -1,9 +1,9 @@
 export default function CoreConcept({ image, title, description }) {
-    return (
-      <li>
-        <img src={image} alt="..." />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    );
-  }
+  return (
+    <li>
+      <img src={image} alt="..." />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
