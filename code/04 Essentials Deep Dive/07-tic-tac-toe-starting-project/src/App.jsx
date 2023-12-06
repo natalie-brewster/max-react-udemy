0bf9b7e05b8279c258f1980769259918e1368903
natalie-brewster <ngbrewster@gmail.com>
2023-12-05 23:47:46 -0800

    beginning tic tac toe

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+
+
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
-  
+  const [ activePlayer, setActivePlayer ] = useState('X');
+
+  // after a square is selected turns will switch
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="natalie" symbol="X" />
+          <Player initialName="player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
